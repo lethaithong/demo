2a50ba2b7b35b50432c236e20fe1ea8f7d44371a
lethaithong <thaithong03071999@gmail.com>
2021-12-18 13:31:38 +0700

    thêm file source chưa hoàn thiện

---
+++ Source .cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cstdlib> 
+#include <ctime> 
+using namespace std;
+
+#define max 10
+int a[max][max];
+int w;
+
+void nhapMang(int mt[max][max], int &n);
+void xuatMang(int mt[max][max], int n);
+void ktlt(int mt[max][max], int n);
+int dem(int mt[max][max], int n);
+void KTVT(int mt[max][max], int n);
+
+void KTVT1(int mt[max][max], int n);
+
+int main()
+{
+
+}
+
+void nhapMang(int mt[max][max], int &n)
+{
+
+}
+
+void xuatMang(int mt[max][max], int n)
+{
+
+}
+
+void ktlt(int mt[max][max], int n)
+{
+
+}
+
+int dem(int mt[max][max], int n)
+{
+
+}
+
+void KTVT(int mt[max][max], int n)
+{
+
+}
+
+void KTVT1(int mt[max][max], int n)
+{
+
+}
+
+
+
+
+
+
+
